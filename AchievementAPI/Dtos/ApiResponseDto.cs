5dac59181c59021a7d13f3cb0bc73d48b8c7a44d
Daniel Domingues <95255561+DanielFilipeNunesDomingues@users.noreply.github.com>
2026-06-26 10:53:33 +0100

    Mover os ficheiros da pasta dtos, para os Models

--- AchievementAPI/Dtos/ApiResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace AchievementAPI.Dtos;
-
-public class ApiResponseDto<T>
-{
-    public bool Success { get; set; } = true;
-    public T? Data { get; set; }
-    public string[]? Errors { get; set; }
-    public string? Message { get; set; }
-}
--- AchievementAPI/Dtos/PagedRequestDto.cs
+++
@@ -1,9 +0,0 @@
-namespace AchievementAPI.Dtos;
-
-public class PagedRequestDto
-{
-    public int PageNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
-    public string? SortBy { get; set; }
-    public string? SortDir { get; set; } = "asc";
-}
